db3e1f8f65c1fa85383c7c2a806671c99d66ebaa
wangl5 <github@liam.eml.cc>
2017-01-23 08:57:30 -0800

    Intake initial commit

--- Chewbawka2017/src/org/team1540/chewbawka/Intake.java
+++ Chewbawka2017/src/org/team1540/chewbawka/Intake.java
@@ -1,7 +1,34 @@
 package org.team1540.chewbawka;
 
-public class Intake {
-	public static void setup() {
+import ccre.channel.*;
+import ccre.ctrl.ExtendedMotorFailureException;
+import ccre.drivers.ctre.talon.TalonExtendedMotor;
+import ccre.frc.FRC;
 
+public class Intake {
+	
+	private static final TalonExtendedMotor intakeTEM = FRC.talonCAN(12);
+	
+	private static final BooleanInput intakeButton = Robot.controlBinding.addBoolean("Toggle Intake");
+    
+	public static void setup() throws ExtendedMotorFailureException {
+		
+		// Make a FloatOutput that controls the intake speed
+		FloatOutput intakeMotor = intakeTEM.simpleControl();
+		
+		// Create a boolean cell that switches the intake
+		BooleanCell intake = new BooleanCell(false);
+		
+		// Set the speed to zero when enabling
+		intake.setWhen(false, FRC.startDisabled.or(FRC.startTele).or(FRC.startAuto).or(FRC.startTest));
+		
+		// Setup intake logic
+		FloatInput intakeSpeed = Robot.mainTuning.getFloat("Main Intake Speed", 1f);
+		
+		intake.onPress(intakeMotor.eventSet(intakeSpeed));
+		intake.onRelease(intakeMotor.eventSet(0f));
+		
+		intakeButton.onPress(intake.eventToggle());
+		
 	}
 }
