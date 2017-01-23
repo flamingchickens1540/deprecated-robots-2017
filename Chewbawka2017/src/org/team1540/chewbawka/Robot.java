6435e92220fe8437a7c6b0160ee65781f8c27af5
wangl5 <github@liam.eml.cc>
2017-01-23 07:40:52 -0800

    Added import extendedMotorFailureException

--- Chewbawka2017/src/org/team1540/chewbawka/Robot.java
+++ Chewbawka2017/src/org/team1540/chewbawka/Robot.java
@@ -1,26 +1,27 @@
 package org.team1540.chewbawka;
 
+import ccre.ctrl.ExtendedMotorFailureException;
 import ccre.ctrl.binding.ControlBindingCreator;
 import ccre.frc.FRC;
 import ccre.frc.FRCApplication;
 import ccre.tuning.TuningContext;
 
 public class Robot implements FRCApplication {
-	
+
 	public static final ControlBindingCreator controlBinding = FRC.controlBinding();
 	public static final TuningContext mainTuning = new TuningContext("MainTuning").publishSavingEvent();
 
-    public static final int TEAM_NUMBER = 1540;
+	public static final int TEAM_NUMBER = 1540;
 
-    @Override
-    public void setupRobot() {
-        Climber.setup();
-        DriveTrain.setup();
-        GearSlider.setup();
-        Hopper.setup();
-        Intake.setup();
-        Shooter.setup();
-        
-        Autonomous.setup();
-    }
+	@Override
+	public void setupRobot() throws ExtendedMotorFailureException {
+		Climber.setup();
+		DriveTrain.setup();
+		GearSlider.setup();
+		Hopper.setup();
+		Intake.setup();
+		Shooter.setup();
+
+		Autonomous.setup();
+	}
 }
