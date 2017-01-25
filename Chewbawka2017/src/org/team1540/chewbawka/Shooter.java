2677c2767ec980fc7ab828ba73c6f25641e71da9
wangl5 <github@liam.eml.cc>
2017-01-24 21:39:51 -0800

    Added PID for the shooter belt and added shooter front conveyor and funneling rollers

--- Chewbawka2017/src/org/team1540/chewbawka/Shooter.java
+++ Chewbawka2017/src/org/team1540/chewbawka/Shooter.java
@@ -7,10 +7,14 @@ import ccre.drivers.ctre.talon.TalonExtendedMotor;
 import ccre.frc.FRC;
 
 public class Shooter {
-	private static final TalonExtendedMotor shooterBelt = FRC.talonCAN(7);
-	private static final TalonExtendedMotor shooterIntake = FRC.talonCAN(8);
-	private static final TalonExtendedMotor hopperAgitator = FRC.talonCAN(9);
+	private static final TalonExtendedMotor flywheelLeft = FRC.talonCAN(10);
+	private static final TalonExtendedMotor flywheelRight = FRC.talonCAN(11);
 	
+	private static final TalonExtendedMotor shooterBelt = FRC.talonCAN(13);
+	private static final TalonExtendedMotor shooterFrontConveyor = FRC.talonCAN(14);
+	private static final TalonExtendedMotor shooterFunnelingRollerLeft = FRC.talonCAN(15);
+	private static final TalonExtendedMotor shooterFunnelingRollerRight = FRC.talonCAN(16);
+
 	private static final BooleanInput fireButton = Robot.controlBinding.addBoolean("Shooter Fire");
 	private static final BooleanInput cancelButton = Robot.controlBinding.addBoolean("Shooter eStop");
 
@@ -42,19 +46,29 @@ public class Shooter {
 				intakeShootingSpeed, // firing
 				FloatInput.zero); // compensate
 
+		// Set the target velocity of the belt
+		FloatInput beltTargetShootingVelocity = Robot.mainTuning.getFloat("Shooter Belt Target Velocity", 1000f);
+
+		FloatInput beltTargetVelocity = shooterStates.selectByState(
+				FloatInput.zero, // passive
+				FloatInput.zero, // spinup
+				beltTargetShootingVelocity, // firing
+				FloatInput.zero); // compensate
+
 		// Control bindings
-		
-		BooleanCell cancel = new BooleanCell(false);
-		fireButton.onRelease().send(cancel.eventSet(true));
+		fireButton.onRelease().send(shooterStates.getStateSetEvent("passive"));
 
 		// Setup flywheel PID controller
-		TalonExtendedMotor flywheelRight = FRC.talonCAN(10);
-		TalonExtendedMotor flywheelLeft = FRC.talonCAN(11);
+		
 		flywheelLeft.modGeneralConfig().configureReversed(false, false);
 		flywheelLeft.modGeneralConfig().activateFollowerMode(flywheelRight);
 
 		PIDTalon flywheelTalon = new PIDTalon(flywheelRight, "Shooter Flywheel", flywheelTargetVelocity);
 		flywheelTalon.setup();
+		
+		// Setup belt PID controller 
+		PIDTalon beltTalon = new PIDTalon(shooterBelt, "Shooter Belt", beltTargetVelocity);
+		beltTalon.setup();
 
 		// Set tunable variables
 		FloatInput shooterSlowThreshold = Robot.mainTuning.getFloat("Shooter Slowdown Threshold", 2100f);
@@ -64,22 +78,18 @@ public class Shooter {
 		flywheelTalon.isUpToSpeed.onPress().and(shooterStates.getIsState("spinup")).send(shooterStates.getStateSetEvent("firing"));
 		flywheelTalon.velocity.atMost(shooterSlowThreshold).onPress().and(shooterStates.getIsState("firing")).send(shooterStates.getStateSetEvent("compensate"));
 		flywheelTalon.velocity.atLeast(flywheelShootingVelocity).onPress().and(shooterStates.getIsState("compensate")).send(shooterStates.getStateSetEvent("firing"));
-		shooterStates.onEnterState("spinup").and(cancel).send(cancel.eventSet(false));
-		shooterStates.onEnterState("compensate").and(cancel).send(cancel.eventSet(false));
-		cancel.onRelease().send(shooterStates.getStateSetEvent("passive"));
-		cancelButton.onPress(shooterStates.getStateSetEvent("passive"));
 
 		// Reset the state machine when switching modes
 		shooterStates.setStateWhen("passive", FRC.startDisabled.or(FRC.startTele).or(FRC.startAuto).or(FRC.startTest));
 
 		// Send intake speeds
-		FloatInput shooterBeltConstant = Robot.mainTuning.getFloat("Shooter Belt Constant", .5f);
-		FloatInput shooterIntakeConstant = Robot.mainTuning.getFloat("Shooter Intake Constant", .5f);
-		FloatInput hopperAgitatorConstant = Robot.mainTuning.getFloat("Shooter Agitator Constant", 1f);
+		FloatInput shooterFrontConveyorConstant = Robot.mainTuning.getFloat("Shooter Front Conveyor Constant", .5f);
+		FloatInput shooterFunnelingRollerConstant = Robot.mainTuning.getFloat("Shooter Funneling Roller Constant", 1f);
 		
-		intakeSpeed.withRamping(.02f, FRC.constantPeriodic).multipliedBy(shooterBeltConstant).send(shooterBelt.simpleControl());
-		intakeSpeed.withRamping(.02f, FRC.constantPeriodic).multipliedBy(shooterIntakeConstant).send(shooterIntake.simpleControl());
-		intakeSpeed.withRamping(.02f, FRC.constantPeriodic).multipliedBy(hopperAgitatorConstant).send(hopperAgitator.simpleControl());
+		FloatOutput shooterFunnelingRoller = shooterFunnelingRollerLeft.simpleControl().combine(shooterFunnelingRollerRight.simpleControl().negate());
+		
+		intakeSpeed.multipliedBy(shooterFrontConveyorConstant).send(shooterFrontConveyor.simpleControl().addRamping(.02f, FRC.constantPeriodic));
+		intakeSpeed.multipliedBy(shooterFunnelingRollerConstant).send(shooterFunnelingRoller.addRamping(.02f, FRC.constantPeriodic));
 
 		// Publish
 		Cluck.publish("flywheelTargetVelocity", flywheelTargetVelocity);
