1e05c0fc19de0c78829f5826efb1ae03db8b72e3
wangl5 <github@liam.eml.cc>
2017-01-25 16:06:50 -0800

    Improved organization

--- Chewbawka2017/src/org/team1540/chewbawka/DriveTrain.java
+++ Chewbawka2017/src/org/team1540/chewbawka/DriveTrain.java
@@ -8,8 +8,8 @@ import ccre.frc.FRC;
 
 public class DriveTrain {
 
-	private static final TalonExtendedMotor[] rightCANs = new TalonExtendedMotor[] { FRC.talonCAN(4), FRC.talonCAN(5), FRC.talonCAN(6) };
-	private static final TalonExtendedMotor[] leftCANs = new TalonExtendedMotor[] { FRC.talonCAN(1), FRC.talonCAN(2), FRC.talonCAN(3) };
+	public static final TalonExtendedMotor[] rightCANs = new TalonExtendedMotor[] { FRC.talonCAN(4), FRC.talonCAN(5), FRC.talonCAN(6) };
+	public static final TalonExtendedMotor[] leftCANs = new TalonExtendedMotor[] { FRC.talonCAN(1), FRC.talonCAN(2), FRC.talonCAN(3) };
 
 	private static final FloatInput leftDriveControls = Robot.controlBinding.addFloat("Drive Left Axis").deadzone(0.2f);
 	private static final FloatInput rightDriveControls = Robot.controlBinding.addFloat("Drive Right Axis").deadzone(0.2f);
@@ -21,16 +21,25 @@ public class DriveTrain {
 	public static void setup() throws ExtendedMotorFailureException {
 
 		// Combine right and left motors into FloatOutputs
-		FloatOutput rightMotors = rightCANs[0].simpleControl().combine(rightCANs[1].simpleControl()).combine(rightCANs[2].simpleControl());
-		FloatOutput leftMotors = leftCANs[0].simpleControl().combine(leftCANs[1].simpleControl()).combine(leftCANs[2].simpleControl()).negate();
+		FloatOutput rightMotors = rightCANs[0].simpleControl()
+				.combine(rightCANs[1].simpleControl())
+				.combine(rightCANs[2].simpleControl());
+		
+		FloatOutput leftMotors = leftCANs[0].simpleControl()
+				.combine(leftCANs[1].simpleControl())
+				.combine(leftCANs[2].simpleControl()).negate();
 
 		// Set the speed to zero when when enabling
 		rightMotors.setWhen(0, FRC.startDisabled.or(FRC.startTele).or(FRC.startAuto).or(FRC.startTest));
 		leftMotors.setWhen(0, FRC.startDisabled.or(FRC.startTele).or(FRC.startAuto).or(FRC.startTest));
 
-		// Send
+		// Send some stuff
 		FloatInput extended = extendedForwards.minus(extendedBackwards);
 
-		Drive.extendedTank(leftDriveControls, rightDriveControls, extended, leftMotors.addRamping(driveRampingConstant.get(), FRC.constantPeriodic), rightMotors.addRamping(driveRampingConstant.get(), FRC.constantPeriodic));
+		Drive.extendedTank(leftDriveControls, 
+				rightDriveControls, 
+				extended, 
+				leftMotors.addRamping(driveRampingConstant.get(), FRC.constantPeriodic), 
+				rightMotors.addRamping(driveRampingConstant.get(), FRC.constantPeriodic));
 	}
 }
