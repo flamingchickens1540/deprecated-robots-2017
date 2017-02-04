4cc6eca39151ba2a6c21ddc66573be2dc3324250
Amber Merrill <merrillam@catlin.edu>
2017-02-04 14:36:02 -0800

    reorganized slider calibration code

--- Chewbawka2017/src/org/team1540/chewbawka/GearSlider.java
+++ Chewbawka2017/src/org/team1540/chewbawka/GearSlider.java
@@ -11,6 +11,7 @@ import ccre.channel.FloatOutput;
 import ccre.cluck.Cluck;
 import ccre.ctrl.ExtendedMotor.OutputControlMode;
 import ccre.ctrl.ExtendedMotorFailureException;
+import ccre.ctrl.StateMachine;
 import ccre.drivers.ctre.talon.TalonExtendedMotor;
 import ccre.frc.FRC;
 
@@ -28,6 +29,13 @@ public class GearSlider {
 		depositingGear.onPress(() -> {servoLeft.set(0); servoRight.set(120);});
 		depositingGear.onRelease(() -> {servoLeft.set(120); servoLeft.set(0);});
 		
+		StateMachine gearSliderStates = new StateMachine(0,
+				"not calibrated",
+				"calibrating 0 end",
+				"calibrating distance",
+				"returning to center",
+				"calibrated");
+		
 		gearSliderMotor.modEncoder().configureEncoderCodesPerRev(125 * 15);
 		gearSliderMotor.modGeneralConfig().configureMaximumOutputVoltage(12f, -12f);
 		FloatCell calibratingSpeed = new FloatCell();
@@ -36,47 +44,48 @@ public class GearSlider {
 		FloatIO gearSliderPosition = gearSliderMotor.modEncoder().getEncoderPosition();
 		FloatInput gearSliderVelocity = gearSliderMotor.modEncoder().getEncoderVelocity();
 		
+		FloatCell calibrationSpeed = new FloatCell(10f);
 		FloatCell slow = new FloatCell(2f);
 		BooleanInput tooSlow = gearSliderVelocity.absolute().atMost(slow);
-		BooleanCell backward1 = new BooleanCell();
-		BooleanCell forward = new BooleanCell();
-		BooleanCell backward2 = new BooleanCell();
 		FloatCell slidingDistance = new FloatCell();
 		BooleanCell calibrated = new BooleanCell();
 		FloatCell positionError = new FloatCell(10f);
 		BooleanInput atMiddle = gearSliderPosition.inRange(slidingDistance.dividedBy(2f).minus(positionError), 
 				slidingDistance.dividedBy(2f).plus(positionError));
-		
-		EventOutput calibrate = gearSliderSpeedControl.eventSet(calibratingSpeed.negated()).combine(
-				() -> {backward1.set(true); forward.set(false); backward2.set(false);});
-		EventOutput sliderForward = gearSliderSpeedControl.eventSet(calibratingSpeed).combine(
-				() -> {forward.set(true); backward1.set(false);});
-		EventOutput sliderBackward2 = gearSliderSpeedControl.eventSet(calibratingSpeed.negated()).combine(
-				() -> {forward.set(false); backward2.set(true);});
 		EventOutput resetEncoder = gearSliderPosition.eventSet(0f);
 		EventOutput recordDistance = slidingDistance.eventSet(gearSliderPosition);
-		EventOutput sliderStop = gearSliderSpeedControl.eventSet(0f);
 		
-		tooSlow.onPress().and(backward1).send(resetEncoder.combine(sliderForward));
-		tooSlow.onPress().and(forward).send(recordDistance.combine(sliderBackward2));
-		atMiddle.onPress().and(backward2).send(sliderStop);
+		EventCell calibrate = new EventCell();
+		gearSliderStates.setStateWhen("calibrating 0 end", calibrate);
+		gearSliderStates.setStateWhen("calibrating distance", 
+				tooSlow.onPress().and(gearSliderStates.getIsState("calibrating 0 end")));
+		
+		calibrate.on(FRC.startTele);
+		gearSliderSpeedControl.setWhen(calibrationSpeed.negated(), 
+				gearSliderStates.onEnterState("calibrating 0 end").or(gearSliderStates.onEnterState("returning to center")));
+		gearSliderSpeedControl.setWhen(calibrationSpeed, gearSliderStates.onEnterState("calibrating distance"));
+		gearSliderSpeedControl.setWhen(0f, gearSliderStates.onEnterState("calibrated"));
+		resetEncoder.on(gearSliderStates.onExitState("calibrating 0"));
+		recordDistance.on(gearSliderStates.onExitState("calibrating distance"));
+		calibrated.setWhen(true, gearSliderStates.onExitState("calibrating distance"));
 		
 		BooleanInput sliderTooFarF = gearSliderPosition.atLeast(slidingDistance);
 		BooleanInput sliderTooFarB = gearSliderPosition.atMost(0f);
 		sliderTooFarF.onPress(gearSliderPositionControl.eventSet(slidingDistance));
 		sliderTooFarB.onPress(gearSliderPositionControl.eventSet(0f));
 		FloatCell slidingControlScaling = new FloatCell(1f);
-		ControlBindings.gearSliderControls.onChange().send(gearSliderPositionControl.eventSet(
+		ControlBindings.gearSliderControls.onChange().and(calibrated).send(gearSliderPositionControl.eventSet(
 				gearSliderPosition.plus(ControlBindings.gearSliderControls.multipliedBy(slidingControlScaling))));
 		
 		Cluck.publish("Gear Servo Left Output", servoLeft);
 		Cluck.publish("Gear Servo Right Output", servoRight);
 		Cluck.publish("Gear Depositing", depositingGear);
 		Cluck.publish("Gear Slider Calibrate", calibrate);
+		Cluck.publish("Gear Slider Calibrated", calibrated);
 		Cluck.publish("Gear Slider Calibrating Speed", calibratingSpeed);
 		Cluck.publish("Gear Slider Speed Control", gearSliderSpeedControl);
 		Cluck.publish("Gear Slider Position Control", gearSliderPositionControl);
-		Cluck.publish("Gear Slider Too Slow Speed", slow);
+		Cluck.publish("Gear Slider Too Slow Threshold", slow);
 		Cluck.publish("Gear Slider Position Error", positionError);
 		Cluck.publish("Gear Slider Control Scaling", slidingControlScaling);
 		
